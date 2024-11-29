926a6ccb95ae104b0df0e9f1f049c8741f143cc0
bajed <tristansubong@gmail.com>
2024-11-29 23:22:10 +0800

    feat: add a button for event selector

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -19,6 +19,8 @@ class _ScannerPageState extends State<ScannerPage>{
     detectionSpeed: DetectionSpeed.noDuplicates,
   );
 
+  String _output = "none";
+
   @override
   Widget build(BuildContext context){
 
@@ -30,7 +32,7 @@ class _ScannerPageState extends State<ScannerPage>{
 
             final Size layoutSize = constraints.biggest;
             const double scanWindowHeight = 100;
-            const double scanWindowWidth = 400;
+            const double scanWindowWidth = 300;
 
             final Rect scanWindow = Rect.fromCenter(
               center: layoutSize.center(Offset.zero),
@@ -42,25 +44,68 @@ class _ScannerPageState extends State<ScannerPage>{
               controller: controller,
               scanWindow: scanWindow,
               overlayBuilder: (context, constraints) {
-                return Container(
-                  width: scanWindowWidth,
-                  height: scanWindowHeight,
-                  decoration: BoxDecoration(
-                    border: Border.all()
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Align(
+                          child: Container(
+                            width: scanWindowWidth,
+                            height: scanWindowHeight,
+                            decoration: BoxDecoration(
+                              border: Border.all()
+                            ),
+                            child: Text(_output),
+                          ),
+                        )
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            color: Colors.blue[700],
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Fluttertoast.showToast(
+                                    msg: "TAPPED",
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.CENTER,
+                                    timeInSecForIosWeb: 1,
+                                    backgroundColor: Colors.red,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "Event name",
+                                      style: TextStyle(
+                                        fontSize: 30
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Icon(Icons.arrow_downward_rounded)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 );
               },
               onDetect: (barcodes) {
                 String output = barcodes.barcodes.first.displayValue!;
-                Fluttertoast.showToast(
-                  msg: output.substring(1, output.length),
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIosWeb: 1,
-                  backgroundColor: Colors.red,
-                  textColor: Colors.white,
-                  fontSize: 16.0
-                );
+
+                setState(() {
+                  _output = output.substring(1, output.length);
+                });
+
               },
             );
           }
