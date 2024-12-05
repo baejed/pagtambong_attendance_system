5a2f7166d612d06803ff9c4aa2e23e9d95a9cc19
bajed <tristansubong@gmail.com>
2024-12-05 22:08:23 +0800

    feat: scanner search database

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pagtambong_attendance_system/generic_component.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -19,7 +21,13 @@ class _ScannerPageState extends State<ScannerPage>{
     detectionSpeed: DetectionSpeed.noDuplicates,
   );
 
+  final CollectionReference _studentsDb = FirebaseFirestore.instance.collection('student-info');
+  final CollectionReference _eventsDB = FirebaseFirestore.instance.collection('events');
+
+
+  String _selectedEvent = "Select an event";
   String _output = "none";
+  bool _scanned = false;
 
   @override
   Widget build(BuildContext context){
@@ -62,33 +70,67 @@ class _ScannerPageState extends State<ScannerPage>{
                       ),
                       Column(
                         children: [
-                          Container(
-                            color: Colors.blue[700],
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Fluttertoast.showToast(
-                                    msg: "TAPPED",
-                                    toastLength: Toast.LENGTH_SHORT,
-                                    gravity: ToastGravity.CENTER,
-                                    timeInSecForIosWeb: 1,
-                                    backgroundColor: Colors.red,
-                                    textColor: Colors.white,
-                                    fontSize: 16.0
-                                  );
-                                },
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "Event name",
-                                      style: TextStyle(
-                                        fontSize: 30
+                          GestureDetector(
+                            onTap: () {
+                              showModalBottomSheet<void>(
+                                context: context,
+                                builder: (BuildContext context) {
+
+                                  final Stream<QuerySnapshot> streamEventsDB = _eventsDB.where('is_open', isEqualTo: true).snapshots();
+
+                                  return Padding(
+                                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+                                    child: SizedBox(
+                                      height: 200,
+                                      child: StreamBuilder(
+                                        stream: streamEventsDB,
+                                        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                                    
+                                          if(streamSnapshot.hasData) {
+                                            return ListView.builder(
+                                              padding: const EdgeInsets.all(10),
+                                              itemCount: streamSnapshot.data!.docs.length,
+                                              itemBuilder: (context, index) {
+                                                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                                                return Material(child: ListTile(
+                                                  title: Text(documentSnapshot['event_name']),
+                                                  subtitle: Text(documentSnapshot['venue']),  
+                                                  onTap: () {
+                                                    setState(() {
+                                                      _selectedEvent = documentSnapshot['event_name'];
+                                                    });
+                                                    Navigator.pop(context);
+                                                  },
+                                                ));
+                                            });
+                                          }
+                                    
+                                          return const Center(child: CircularProgressIndicator(),);
+                                    
+                                        },
                                       ),
                                     ),
-                                    Spacer(),
-                                    Icon(Icons.arrow_downward_rounded)
-                                  ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Container(
+                              color: Colors.blue[700],
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: GestureDetector(
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        _selectedEvent,
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      const Spacer(),
+                                      const Icon(Icons.arrow_downward_rounded)
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
@@ -100,10 +142,105 @@ class _ScannerPageState extends State<ScannerPage>{
                 );
               },
               onDetect: (barcodes) {
+                if(_scanned) return;
+                if(_selectedEvent == "Select an event") {
+                  Fluttertoast.showToast(
+                    msg: "Please select an event",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 16.0
+                  );
+                  return;
+                }
                 String output = barcodes.barcodes.first.displayValue!;
+                output = output.substring(1, output.length);
+
+                Query students = _studentsDb.where('student_id', isEqualTo: output);
+                Stream<QuerySnapshot> studentSnapshot = students.snapshots();
+
+                students.get().then((value) {
+                  if(value.docs.isNotEmpty){
+                    String firstName = value.docs.first['first_name'];
+
+                    setState(() {
+                      _scanned = true;
+                    });
+
+                    showDialog(
+                      context: context, 
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                            title: Text("Confirmation"),
+                            content: Text(firstName),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  setState(() {
+                                    _scanned = false;
+                                  });
+                                },
+                                child: const Text("Okay"),
+                              )
+                            ],
+                          );
+                      }
+                    );
+                  }
+                });
+
+                // showDialog(
+                //   context: context, 
+                //   barrierDismissible: false,
+                //   builder: (BuildContext context) {
+
+
+                //     return StreamBuilder(
+                //       stream: studentSnapshot, 
+                //       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+
+                //         if(streamSnapshot.hasData && streamSnapshot.data!.docs.isNotEmpty){
+
+                //           String firstName = streamSnapshot.data!.docs.first['first_name'];
+
+                //           return AlertDialog(
+                //             title: Text("Confirmation"),
+                //             content: Text(firstName),
+                //             actions: [
+                //               TextButton(
+                //                 onPressed: () {
+                //                   Navigator.pop(context);
+                //                   setState(() {
+                //                     _scanned = false;
+                //                   });
+                //                 },
+                //                 child: Text("Okay"),
+                //               )
+                //             ],
+                //           );
+                          
+                //         }
+
+                //         if(streamSnapshot.connectionState == ConnectionState.waiting){
+                //           return AlertDialog(
+                //             title: Text("LOADING"),
+                //             content: Text("searching student"),
+                //           );
+                //         }
+
+                //         return SizedBox.shrink();
+
+                //       }
+                //     );
+                //   }
+                // );
 
                 setState(() {
-                  _output = output.substring(1, output.length);
+                  _output = output;
+                  // _scanned = true;
                 });
 
               },
@@ -117,4 +254,48 @@ class _ScannerPageState extends State<ScannerPage>{
     );
   }
 
+}
+
+String showSelectEventModal(BuildContext context){
+
+  String selectedEvent = "Select edvent";
+
+  
+
+  return selectedEvent;
+}
+
+bool _dialogBuilder(BuildContext context, String name) {
+
+  AlertDialog(
+    title: const Text('Basic dialog title'),
+    content: const Text(
+      'A dialog is a type of modal window that\n'
+      'appears in front of app content to\n'
+      'provide critical information, or prompt\n'
+      'for a decision to be made.',
+    ),
+    actions: <Widget>[
+      TextButton(
+        style: TextButton.styleFrom(
+          textStyle: Theme.of(context).textTheme.labelLarge,
+        ),
+        child: const Text('Disable'),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+          textStyle: Theme.of(context).textTheme.labelLarge,
+        ),
+        child: const Text('Enable'),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      ),
+    ],
+  );
+
+  return false;
 }
