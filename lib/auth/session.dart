fb57a71c4a82a8878c5b5cfb480b4a9cf4f90306
LanseylowT <donlancelotknight123@gmail.com>
2025-01-14 05:31:53 +0800

    Fixed the the problem, basically calling async to a function without wrapping the type into a Future that simple function breaks the entire app

--- lib/auth/session.dart
+++ lib/auth/session.dart
@@ -7,15 +7,11 @@ class Session {
   static UserRole? loggedRole;
   static User? user;
 
-  static initRole() async {
+  static Future<void> init() async {
     // if the user is null, it gets the user first
-    if (user == null) {
-      await initUser();
-    }
-    loggedRole = await AuthService().getUserRole(user!.uid);
-  }
-
-  static initUser() async {
     user = await AuthService().getCurrUser();
+    if (user != null) {
+      loggedRole = await AuthService().getUserRole(user!.uid);
+    }
   }
 }
